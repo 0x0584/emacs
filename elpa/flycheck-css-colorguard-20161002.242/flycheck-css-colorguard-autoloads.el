ffbba31249bb2f3f071acef90f1207252f60fb92
Anas Rchid <rchid.anas@gmail.com>
2017-02-18 08:52:53 +0000

    some changes

--- elpa/flycheck-css-colorguard-20161002.242/flycheck-css-colorguard-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; flycheck-css-colorguard-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("flycheck-css-colorguard.el") (22517 19209
-;;;;;;  780365 955000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; flycheck-css-colorguard-autoloads.el ends here
---
+++ elpa/pandoc-20161128.357/pandoc-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "pandoc" "20161128.357" "Pandoc interface" '((emacs "24.4")) :url "https://github.com/zonuexe/pandoc.el" :keywords '("hypermedia" "documentation" "markup" "converter"))
