f2e427633b5ba01e3bf2617867c9a17235707e26
Anas Rchid <rchid.anas@gmail.com>
2017-10-27 13:45:02 +0100

    fix org-mode and plantuml/Graphviz/Dita to export in pdf

--- elpa/apiwrap-20171019.916/apiwrap-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "apiwrap" "20171019.916" "api-wrapping macros" '((emacs "25")) :commit "5d25972192cd34553997ba193c09eab093a2b870" :url "https://github.com/vermiculus/apiwrap.el" :keywords '("tools" "maint" "convenience"))
--- init.el
+++ init.el
@@ -5,8 +5,9 @@
 ;;(add-hook 'after-init-hook #'global-emojify-mode)
 
 ;; ===== INSERT TODAY'S DATE AND TIME ====
-;;(defun insert-current-date () (interactive)
-;;       (insert (shell-command-to-string "echo -n $(date +%m/%d/%Y-%H:%M:%S)")))
+(defun insert-current-date () 
+  (interactive)
+  (insert (shell-command-to-string "echo -n $(date +%m/%d/%Y-%H:%M:%S)")))
 (defun insert-date-time ()
   "Insert current date mm/dd/yyyy_H:M:S."
   (interactive)
@@ -159,10 +160,12 @@
  '(org-agenda-files
    (quote
     ("~/orged/LP-DBA/gl.org" "/home/arfed/orged/MASTER.org" "/home/arfed/orged/Q.org" "/home/arfed/orged/emails.org" "/home/arfed/orged/ghiwan.org" "/home/arfed/orged/led-zeppelin.org" "/home/arfed/orged/todo.org")))
+ '(org-plantuml-jar-path "/home/arfed/.emacs.d/plantuml/plantuml.jar")
  '(package-selected-packages
    (quote
     (smartparens tern zenburn-theme php+-mode yascroll visual-regexp vkill minor-mode-hack symon magithub function-args bongo emms smart-mode-line smart-mode-line-powerline-theme helm-dictionary define-word dictionary nhexl-mode quickrun popwin ace-popup-menu popup-complete popup-kill-ring popup-switcher basic-c-compile company company-irony-c-headers flycheck-irony irony irony-eldoc runtests rvm rsense ac-inf-ruby flymake-ruby rubocop yari helm-rubygems-local helm-rubygems-org helm-spotify howdoi howm omniref ruby-block ruby-compilation ruby-dev ruby-electric ruby-factory ruby-hash-syntax ruby-refactor ruby-tools helm-robe robe swift3-mode benchmark-init ivy-youtube sudoku zweilight-theme helm-gitignore hide-comnt super-save ggtags desktop+ ac-html ac-clang flycheck-css-colorguard flymake-css all-the-icons ox-pandoc pandoc pandoc-mode langtool rich-minority diminish auto-complete-auctex flylisp c-eldoc term+ markdown-mode+ org-commentary org-doing org-readme orgtbl-ascii-plot spaces ssh svg svg-clock swap-buffers sx vline vmd-mode org-pandoc highlight highlight-quoted highlight-escape-sequences highlight-operators highlight-blocks plantuml-mode ascii-art-to-unicode smex desktop-menu magit-filenotify magit-rockstar latex-preview-pane magic-latex-buffer tango-2-theme tangotango-theme anti-zenburn-theme auctex git-timemachine zeal-at-point dash-at-point linum-relative bm ac-helm dark-krystal-theme caroline-theme meacupla-theme clues-theme cherry-blossom-theme distinguished-theme soothe-theme grandshell-theme company-irony company-c-headers helm-company helm-make helm-themes electric-operator flycheck-perl6 rainbow-delimiters helm-gtags ctags-update hl-sexp rainbow-blocks ctags clang-format bind-key flycheck flycheck-cstyle iedit impatient-mode xkcd rotate nyan-mode inkpot-theme imgur hlinum gnuplot gnu-apl-mode flycheck-clangcheck emojify elfeed disaster chess badger-theme auto-complete-clang auto-complete-c-headers ac-c-headers abyss-theme 2048-game 0blayout)))
  '(pdf-view-midnight-colors (quote ("#232333" . "#c7c7c7")))
+ '(plantuml-jar-path "/home/arfed/.emacs.d/plantuml/plantuml.jar")
  '(powerline-color1 "#1E1E1E")
  '(powerline-color2 "#111111")
  '(puml-plantuml-jar-path "~/.emacs.d/plantuml/plantuml.jar")
@@ -280,6 +283,17 @@
 ;; '(rainbow-delimiters-depth-8-face ((t (:inherit outline-7 :foreground "#CC681F"))))
 ;; '(rainbow-delimiters-depth-9-face ((t (:inherit outline-8 :foreground "#4B0082"))))
 
+
+(require 'company)
+(add-hook 'global-init-hook 'global-company-mode)
+(add-to-list 'company-backends 'company-c-headers)
+
+(add-hook 'c++-mode-hook 'irony-mode)
+(add-hook 'c-mode-hook 'irony-mode)
+(add-hook 'objc-mode-hook 'irony-mode)
+
+(add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
+
 
 ;; ==== MOVE LINES ====
 (defun move-line (n)
@@ -328,37 +342,54 @@
          ";;; Commentary:\n\n"
          ";;; Code:\n\n"
          ";;; " (file-name-nondirectory buffer-file-name) " ends here\n")
-        ((c-mode . "C program") nil
-         
-         "/**************************************************************************\\ \n"
-         " File: " (file-name-nondirectory buffer-file-name) "\n\n"
-	 "The MIT License (MIT)\n
+        ;; ((c-mode . "C program") nil
+	
+	;;          "/**************************************************************************\\ \n"
+	;;          " File: " (file-name-nondirectory buffer-file-name) "\n\n"
+	;; 	 "The MIT License (MIT)\n
 
-Copyright (c)" (substring (current-time-string) -4)" - Anas Rchid
+	;; Copyright (c)" (substring (current-time-string) -4)" - Anas Rchid
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
- of this software and associated documentation files (the ""Software""), to 
-deal in the Software without restriction, including without limitation the 
-rights to use, copy, modify, merge, publish, distribute, sublicense, and/or 
-sell copies of the Software, and to permit persons to whom the Software is 
-furnished to do so, subject to the following conditions:
+	;; Permission is hereby granted, free of charge, to any person obtaining a copy
+	;;  of this software and associated documentation files (the ""Software""), to 
+	;; deal in the Software without restriction, including without limitation the 
+	;; rights to use, copy, modify, merge, publish, distribute, sublicense, and/or 
+	;; sell copies of the Software, and to permit persons to whom the Software is 
+	;; furnished to do so, subject to the following conditions:
 
-The above copyright notice and this permission notice shall be included in 
-all copies or substantial portions of the Software.
+	;; The above copyright notice and this permission notice shall be included in 
+	;; all copies or substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED ""AS IS"", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM
-, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN 
-THE SOFTWARE.
+	;; THE SOFTWARE IS PROVIDED ""AS IS"", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+	;; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+	;; FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+	;; AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+	;; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM
+	;; , OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN 
+	;; THE SOFTWARE.
 
- Last modicication:						
-	"(insert-date)"					
-								
-\\**************************************************************************/\n"
-	" \n\n")
+	;;  Last modicication:						
+	;; 	"(insert-date)"					
+	
+	;; \\**************************************************************************/\n"
+	;; 	" \n\n")
+	((c-mode . "C program") nil
+	 "/* FILE: \t" (file-name-nondirectory buffer-file-name) "\n"
+	 " * AUTHOR: \tANAS RCHID\n"
+	 " * DESCRIPTION:\tNULL\n"
+	 " * \n"
+	 " * CREATION:\t" (insert-date) "\n"
+	 " * MODIFIED:\t" (insert-date) " */\n"
+	 "/* ---------- headers ---------- */\n"
+	 "#include <stdio.h>\n#include<stdlib.h>\n\n"
+	 "/* ---------- types ---------- */\n\n"
+	 "/* ---------- prototypes ---------- */\n\n"
+	 "/* ---------- main entry ---------- */\n"
+	 "int main(int argc, char **argv) {\n"
+	 "  return 0;\n"
+	 "}\n\n"
+	 "/* ---------- implementations ---------- */\n"
+	 )
         ((shell-mode . "Shell script") nil
          "#!/bin/bash\n\n"
          " # File: " (file-name-nondirectory buffer-file-name) "\n"
@@ -368,24 +399,24 @@ THE SOFTWARE.
 
 
 ;; ==== etags ====
- (defun create-tags (dir-name)
-     "Create tags file."
-     (interactive "DDirectory: ")
-     (eshell-command 
-      (format "find . -name '*.c' -print -or -name '*.h' -print | xargs etags --append" dir-name)))
+(defun create-tags (dir-name)
+  "Create tags file."
+  (interactive "DDirectory: ")
+  (eshell-command 
+   (format "find . -name '*.c' -print -or -name '*.h' -print | xargs etags --append" dir-name)))
 
 (defadvice find-tag (around refresh-etags activate)
-   "Rerun etags and reload tags if tag not found and redo find-tag.              
+  "Rerun etags and reload tags if tag not found and redo find-tag.              
    If buffer is modified, ask about save before running etags."
   (let ((extension (file-name-extension (buffer-file-name))))
     (condition-case err
-    ad-do-it
+	ad-do-it
       (error (and (buffer-modified-p)
-          (not (ding))
-          (y-or-n-p "Buffer is modified, save it? ")
-          (save-buffer))
-         (er-refresh-etags extension)
-         ad-do-it))))
+		  (not (ding))
+		  (y-or-n-p "Buffer is modified, save it? ")
+		  (save-buffer))
+	     (er-refresh-etags extension)
+	     ad-do-it))))
 
 (defun er-refresh-etags (&optional extension)
   "Run etags on all peer files in current dir and reload them silently."
@@ -704,7 +735,7 @@ Don't mess with special buffers."
 (iimage-mode)
 ;; add the org file link format to the iimage mode regex
 (add-to-list 'iimage-mode-image-regex-alist
-  (cons (concat "\\[\\[file:\\(~?" iimage-mode-image-filename-regex "\\)\\]")  1))
+	     (cons (concat "\\[\\[file:\\(~?" iimage-mode-image-filename-regex "\\)\\]")  1))
 ;;  add a hook so we can display images on load
 (add-hook 'org-mode-hook '(lambda () (org-turn-on-iimage-in-org)))
 ;; function to setup images for display on load
@@ -719,7 +750,7 @@ Don't mess with special buffers."
   (interactive)
   (if (face-underline-p 'org-link)
       (set-face-underline-p 'org-link nil)
-      (set-face-underline-p 'org-link t))
+    (set-face-underline-p 'org-link t))
   (call-interactively 'iimage-mode))
 (setq org-image-actual-width (/ (display-pixel-width) 3))
 
@@ -777,44 +808,45 @@ Don't mess with special buffers."
 (rich-minority-mode 1)
 (setf rm-blacklist "")
 
-(defvar auto-minor-mode-alist ()
-  "Alist of filename patterns vs correpsonding minor mode functions, see `auto-mode-alist'
-All elements of this alist are checked, meaning you can enable multiple minor modes for the same regexp.")
-(defun enable-minor-mode-based-on-extension ()
-  "check file name against auto-minor-mode-alist to enable minor modes
-the checking happens for all pairs in auto-minor-mode-alist"
-  (when buffer-file-name
-    (let ((name buffer-file-name)
-          (remote-id (file-remote-p buffer-file-name))
-          (alist auto-minor-mode-alist))
-      ;; Remove backup-suffixes from file name.
-      (setq name (file-name-sans-versions name))
-      ;; Remove remote file name identification.
-      (when (and (stringp remote-id)
-                 (string-match-p (regexp-quote remote-id) name))
-        (setq name (substring name (match-end 0))))
-      (while (and alist (caar alist) (cdar alist))
-        (if (string-match (caar alist) name)
-            (funcall (cdar alist) 1))
-        (setq alist (cdr alist))))))
+;; (defvar auto-minor-mode-alist ()
+;;   "Alist of filename patterns vs correpsonding minor mode functions, see `auto-mode-alist'
+;; All elements of this alist are checked, meaning you can enable multiple minor modes for the same regexp.")
 
-(add-hook 'find-file-hook 'enable-minor-mode-based-on-extension)
+;; (defun enable-minor-mode-based-on-extension ()
+;;   "check file name against auto-minor-mode-alist to enable minor modes
+;; the checking happens for all pairs in auto-minor-mode-alist"
+;;   (when buffer-file-name
+;;     (let ((name buffer-file-name)
+;;           (remote-id (file-remote-p buffer-file-name))
+;;           (alist auto-minor-mode-alist))
+;;       ;; Remove backup-suffixes from file name.
+;;       (setq name (file-name-sans-versions name))
+;;       ;; Remove remote file name identification.
+;;       (when (and (stringp remote-id)
+;;                  (string-match-p (regexp-quote remote-id) name))
+;;         (setq name (substring name (match-end 0))))
+;;       (while (and alist (caar alist) (cdar alist))
+;;         (if (string-match (caar alist) name)
+;;             (funcall (cdar alist) 1))
+;;         (setq alist (cdr alist))))))
 
-(define-minor-mode minor-mode-blackout-mode
-  "Hides minor modes from the mode line."
-  t)
+;; (add-hook 'find-file-hook 'enable-minor-mode-based-on-extension)
 
-(catch 'done
-  (mapc (lambda (x)
-          (when (and (consp x)
-                     (equal (cadr x) '("" minor-mode-alist)))
-            (let ((original (copy-sequence x)))
-              (setcar x 'minor-mode-blackout-mode)
-              (setcdr x (list "" original)))
-            (throw 'done t)))
-        mode-line-modes))
+;; (define-minor-mode minor-mode-blackout-mode
+;;   "Hides minor modes from the mode line."
+;;   t)
 
-(global-set-key (kbd "C-c m") 'minor-mode-blackout-mode)
+;; (catch 'done
+;;   (mapc (lambda (x)
+;;           (when (and (consp x)
+;;                      (equal (cadr x) '("" minor-mode-alist)))
+;;             (let ((original (copy-sequence x)))
+;;               (setcar x 'minor-mode-blackout-mode)
+;;               (setcdr x (list "" original)))
+;;             (throw 'done t)))
+;;         mode-line-modes))
+
+;; (global-set-key (kbd "C-c m") 'minor-mode-blackout-mode)
 
 ;; ==== NEOTREE ====
 ;;(setq neo-theme (if window-system 'icons 'arrow))
@@ -835,19 +867,19 @@ the checking happens for all pairs in auto-minor-mode-alist"
 (global-set-key (kbd "C-c ;") 'make-directory)
 
 (global-set-key (kbd "C-x p") (lambda ()
-			(interactive)
-			(other-window -1)))
+				(interactive)
+				(other-window -1)))
 (global-set-key (kbd "C-x 5 p") (lambda ()
-			(interactive)
-			(other-frame -1)))
+				  (interactive)
+				  (other-frame -1)))
 
 (global-set-key (kbd "C-c C-t") (lambda ()
-			(interactive)
-			(transpose-chars -1)))
+				  (interactive)
+				  (transpose-chars -1)))
 
 (global-set-key (kbd "C-c M-t") (lambda ()
-			(interactive)
-			(transpose-words -1)))
+				  (interactive)
+				  (transpose-words -1)))
 
 (global-set-key (kbd "C-c v") (lambda ()
                                 (interactive)
@@ -856,15 +888,15 @@ the checking happens for all pairs in auto-minor-mode-alist"
 
 (global-set-key (kbd "C-c j") 'join-line)
 (global-set-key (kbd "C-c M-j") (lambda ()
-                                (interactive)
-                                (join-line -1)))
+				  (interactive)
+				  (join-line -1)))
 
 (global-set-key (kbd "C-c d") (lambda ()
                                 (interactive)
                                 (delete-char -1)))
 (global-set-key (kbd "C-c M-d") (lambda ()
-                                (interactive)
-                                (kill-word -1)))
+				  (interactive)
+				  (kill-word -1)))
 
 (global-set-key (kbd "C-c e") (lambda ()
                                 (interactive)
@@ -904,19 +936,19 @@ the checking happens for all pairs in auto-minor-mode-alist"
   "Set the windows size according to resolution."
   (interactive)
   (if window-system
-  (progn
-    ;; use 120 char wide window for largeish displays
-    ;; and smaller 80 column windows for smaller displays
-    (if (> (x-display-pixel-width) 1280)
-           (add-to-list 'default-frame-alist (cons 'width 120))
-           (add-to-list 'default-frame-alist (cons 'width 80)))
-    ;; for the height, subtract a couple hundred pixels
-    ;; from the screen height (for panels, menubars and
-    ;; whatnot), then divide by the height of a char to
-    ;; get the height
-    (add-to-list 'default-frame-alist 
-         (cons 'height (/ (- (x-display-pixel-height) 200)
-                             (frame-char-height)))))))
+      (progn
+	;; use 120 char wide window for largeish displays
+	;; and smaller 80 column windows for smaller displays
+	(if (> (x-display-pixel-width) 1280)
+	    (add-to-list 'default-frame-alist (cons 'width 120))
+	  (add-to-list 'default-frame-alist (cons 'width 80)))
+	;; for the height, subtract a couple hundred pixels
+	;; from the screen height (for panels, menubars and
+	;; whatnot), then divide by the height of a char to
+	;; get the height
+	(add-to-list 'default-frame-alist 
+		     (cons 'height (/ (- (x-display-pixel-height) 200)
+				      (frame-char-height)))))))
 
 ;;(set-frame-size-according-to-resolution)
 (autoload 'inf-ruby-minor-mode "inf-ruby" "Run an inferior Ruby process" t)
@@ -1017,16 +1049,24 @@ The themes will be loaded at the specified time every day.")
 (setq org-agenda-files (list "~/orged"))
 
 ;; By an unknown contributor
-          
-(global-set-key "%" 'match-paren)
 
-(defun match-paren (arg)
-  "Go to the matching parenthesis if on parenthesis, otherwise insert %.
+(global-set-key "%" 'match-paren)
+;; (global-set-key ")" 'goto-paren)
+
+;; (defun goto-paren (ARG)
+;;   "Goto matching paren `ARG` and back to the oroginal."
+;;   (interactive "p")
+;;   (cond ((looking-at "\\s\(") (match-paren "%"))
+;;         ((looking-at "\\s\)") (match-paren "%"))
+;;         (t (self-insert-command (or ARG 1)))))
+
+(defun match-paren (ARG)
+  "Go to the matching parenthesis if on parenthesis `ARG`', otherwise insert %.
 vi style of % jumping to matching brace."
   (interactive "p")
   (cond ((looking-at "\\s\(") (forward-list 1) (backward-char 1))
         ((looking-at "\\s\)") (forward-char 1) (backward-list 1))
-        (t (self-insert-command (or arg 1)))))
+        (t (self-insert-command (or ARG 1)))))
 
 (setq show-paren-when-point-inside-paren t)
 
@@ -1040,6 +1080,53 @@ STR is `current-kill` if unspecified."
             (insert str)
             (shell-command-on-region (point-min) (point-max) cmd nil 'replace)
             (buffer-string))))
+(require 'iimage)
+(autoload 'iimage-mode "iimage" "Support Inline image minor mode." t)
+(autoload 'turn-on-iimage-mode "iimage" "Turn on Inline image minor mode." t)
+(add-to-list 'iimage-mode-image-regex-alist '("@startuml\s+\\(.+\\)" . 1))
 
+;; Rendering plantuml
+(defun plantuml-render-buffer ()
+  (interactive)
+  (message "PLANTUML Start rendering")
+  (shell-command (concat "java -jar ~/.emacs.d/plantuml/plantuml.jar " 
+                         buffer-file-name))
+  (message (concat "PLANTUML Rendered:  " (buffer-name))))
+
+;; Image reloading
+(defun reload-image-at-point ()
+  (interactive)
+  (message "reloading image at point in the current buffer...")
+  (image-refresh (get-text-property (point) 'display)))
+
+;; Image resizing and reloading
+(defun resize-image-at-point ()
+  (interactive)
+  (message "resizing image at point in the current buffer123...")
+  (let* ((image-spec (get-text-property (point) 'display))
+         (file (cadr (member :file image-spec))))
+    (message (concat "resizing image..." file))
+    (shell-command
+     (format "convert -resize %d %s %s " 
+	     (* (window-width (selected-window)) (frame-char-width))
+	     file file))
+    (reload-image-at-point)))
+(org-babel-do-load-languages
+ (quote org-babel-load-languages)
+ (quote ((emacs-lisp . t)
+         (java . t)
+         (dot . t)
+         (ditaa . t)
+         (R . t)
+         (python . t)
+         (ruby . t)
+         (gnuplot . t)
+         (clojure . t)
+         (sh . t)
+         (ledger . t)
+         (org . t)
+         (plantuml . t)
+         (latex . t))))
+(message "done.")
 (provide 'init)
 ;;; init ends here
