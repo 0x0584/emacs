be2e3375dbe1acb19705aec3d8bb8b6bc3cddbf3
Anas Rchid <rchid.anas@gmail.com>
2017-12-22 14:40:43 +0000

    some mysql pkg and configs..

---
+++ elpa/mysql-to-org-20170205.1306/mysql-to-org-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "mysql-to-org" "20170205.1306" "Minor mode to output the results of mysql queries to org tables" '((emacs "24.3") (s "1.11.0")) :commit "d87e9b6117fc0db4b156e8a12550cf9ee4bd692a")
